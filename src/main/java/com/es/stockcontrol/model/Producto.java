14f35c1d80933ebd282561f975c142122e1ef65f
Eliaser <eperyaq205@g.educaand.com>
2024-10-18 18:58:55 +0200

    inicio creacion tablas

--- src/main/java/com/es/stockcontrol/model/Producto.java
+++ src/main/java/com/es/stockcontrol/model/Producto.java
@@ -1,4 +1,32 @@
 package com.es.stockcontrol.model;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Date;
+
 public class Producto {
+
+    private String id;
+
+
+    private String categoria;
+
+
+    private String nombre;
+
+
+    private String descripcion;
+
+    private float precio_sin_iva;
+
+    private float precio_con_iva;
+
+    private Date fechaAlta;
+
+    private int stock;
+
+    @ManyToOne
+    @JoinColumn(name = "proveedor")
+    private Proveedor proveedor;
 }
--- src/main/java/com/es/stockcontrol/model/Proveedor.java
+++ src/main/java/com/es/stockcontrol/model/Proveedor.java
@@ -1,4 +1,20 @@
 package com.es.stockcontrol.model;
 
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
 public class Proveedor {
+
+    private long id;
+
+    private String nombre;
+
+    private String direccion;
+
+
+    @OneToMany
+    private List<Producto> listaProductos;
+
+
 }
