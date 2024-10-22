911520c4ea9489985114a8d78297e28d6fc2ec9d
mgarmorn <mgarmorn@g.educaand.es>
2024-10-22 20:53:10 +0200

    issue + ProductoRepository

--- src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
+++ src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
@@ -1,5 +1,128 @@
 package com.es.stockcontrol.repository;
 
-public class ProductoRepository {
+import com.es.stockcontrol.model.Producto;
+import jakarta.persistence.*;
+import org.hibernate.Session;
 
+import java.util.List;
+
+public class ProductoRepository {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("ProyectoStock");
+    EntityManager em = emf.createEntityManager();
+
+    public Producto createProducto(Producto producto) {
+
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            em.persist(producto);
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return producto;
+    }
+
+    public boolean deleteProductoById(String id) {
+        Producto producto = em.find(Producto.class, id);
+
+        if (producto != null) {
+
+            EntityTransaction transaction = em.getTransaction();
+
+            try {
+                transaction.begin();
+                em.remove(producto);
+                transaction.commit();
+
+                return true;
+
+            } catch (Exception e) {
+                if (transaction.isActive()) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public Producto updateNombreProducto(String id, String nuevoNombre) {
+
+        Producto producto = em.find(Producto.class, id);
+
+        if (producto != null) {
+
+            EntityTransaction transaction = em.getTransaction();
+
+            try {
+                transaction.begin();
+                producto.setNombre(nuevoNombre);
+                em.merge(producto);
+                transaction.commit();
+
+            } catch (Exception e) {
+                if (transaction.isActive()) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        }
+
+
+        return producto;
+    }
+
+    public Producto updateStockProducto(String id, int nuevoStock) {
+
+        Producto producto = em.find(Producto.class, id);
+
+        if (producto != null) {
+
+            EntityTransaction transaction = em.getTransaction();
+
+            try {
+                transaction.begin();
+                producto.setStock(nuevoStock);
+                em.merge(producto);
+                transaction.commit();
+
+            } catch (Exception e) {
+                if (transaction.isActive()) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        }
+
+        return producto;
+    }
+
+
+    public Producto getProductoById(String id) {
+
+        return em.find(Producto.class, id);
+    }
+
+    public List<Producto> getProductoByStock(Boolean stock) {
+
+        String jpql;
+
+        if (stock) {
+            jpql = "SELECT p FROM Producto p WHERE p.stock > 0";
+        } else {
+            jpql = "SELECT p FROM Producto p WHERE p.stock < 0";
+        }
+
+        TypedQuery<Producto> query = em.createQuery(jpql, Producto.class);
+
+        return query.getResultList();
+    }
 }
--- src/main/java/com/es/stockcontrol/service/ProductoService.java
+++ src/main/java/com/es/stockcontrol/service/ProductoService.java
@@ -4,7 +4,7 @@ import com.es.stockcontrol.model.Producto;
 
 public class ProductoService {
 
-    public String generarProductoId(Producto producto) {
+    private String generarProductoId(Producto producto) {
         String categoria = producto.getCategoria().substring(0, 3);
         String nombre = producto.getNombre().substring(0, 3);
         String proveedor = producto.getProveedor().getNombre().substring(0, 3);
