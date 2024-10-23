1b4461e7796db5946523e4805c54f9476d397c2b
Eliaser <eperyaq205@g.educaand.com>
2024-10-23 16:38:48 +0200

    User repository, service y controller hecho aunque no terminado

--- src/main/java/com/es/stockcontrol/controller/impl/UserController.java
+++ src/main/java/com/es/stockcontrol/controller/impl/UserController.java
@@ -4,12 +4,27 @@ package com.es.stockcontrol.controller.impl;
 import com.es.stockcontrol.controller.api.UserControllerAPI;
 import com.es.stockcontrol.model.RespuestaHTTP;
 import com.es.stockcontrol.model.User;
+import com.es.stockcontrol.service.UserService;
 
 public class UserController implements UserControllerAPI {
-
+UserService service = new UserService();
 
     @Override
     public RespuestaHTTP<User> login(String userInput, String passInput) {
-        return null;
+
+        if (userInput.isEmpty() || userInput == null & passInput.isEmpty() || passInput == null){
+
+            return new RespuestaHTTP(404, "Not found", null);
+        }else {
+            User usuarioDentro = service.getUser(userInput);
+
+            if(passInput == usuarioDentro.getContrasenia()){
+                return new RespuestaHTTP(200, "ok", usuarioDentro);
+            }else {
+                return new RespuestaHTTP(404, "Not found",null);
+            }
+
+        }
+
     }
 }
--- src/main/java/com/es/stockcontrol/service/UserService.java
+++ src/main/java/com/es/stockcontrol/service/UserService.java
@@ -1,4 +1,36 @@
 package com.es.stockcontrol.service;
 
+import com.es.stockcontrol.model.User;
+import com.es.stockcontrol.repository.UserRepository;
+
+import java.security.PublicKey;
+
 public class UserService {
+    UserRepository repository = new UserRepository();
+
+    public User insertUser(String nombre, String contrasenia){
+        User crearUsuario = new User(nombre, contrasenia);
+
+        if (crearUsuario != null){
+            User insertarUser = repository.insertUser(crearUsuario);
+
+            if (insertarUser != null){
+                return insertarUser;
+            } else {
+                return null;
+            }
+
+        }else {
+            return null;
+        }
+
+    }
+
+    public User getUser( String nombre){
+
+        return repository.getUser(nombre);
+
+
+    }
+
 }
