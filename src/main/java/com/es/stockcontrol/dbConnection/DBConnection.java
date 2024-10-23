bde15973db480484314f17f4b46a01b41fab7fc8
Camilleri <ssancam391@g.educaand.es>
2024-10-23 10:39:55 +0200

    DBConnection

---
+++ src/main/java/com/es/stockcontrol/dbConnection/DBConnection.java
@@ -0,0 +1,40 @@
+package com.es.stockcontrol.dbConnection;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class DBConnection {
+
+    /**
+     * Singleton. Instancia única de EntityManagerFactory
+     */
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ProyectoStock");
+
+
+    /**
+     * Método para obtener un EntityManager
+     */
+    public static EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
+
+    /**
+     * Método para crear EntityManager
+     */
+    public static void closeEntityManager (EntityManager entityManager){
+        if (entityManager != null && entityManager.isOpen()){
+            entityManager.close();
+        }
+    }
+
+    /**
+     * Método para cerrar EntityManagerFactory al finalizar la aplicación
+     */
+    public static  void closeEntityManagerFactory() {
+        if (entityManagerFactory != null && entityManagerFactory.isOpen()){
+            entityManagerFactory.close();
+        }
+    }
+
+}
