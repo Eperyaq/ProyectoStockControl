ce88cd51eff2e70174dd6242f252142546bca797
Camilleri <ssancam391@g.educaand.es>
2024-10-27 11:21:07 +0100

    Documentación_main

--- src/main/java/com/es/stockcontrol/AppStockControl.java
+++ src/main/java/com/es/stockcontrol/AppStockControl.java
@@ -12,57 +12,57 @@ import com.es.stockcontrol.repository.impl.ProveedorRepository;
 import com.es.stockcontrol.service.impl.ProductoService;
 import com.es.stockcontrol.service.impl.ProveedorService;
 import com.es.stockcontrol.utils.Initializer;
-
 import java.util.List;
 import java.util.Scanner;
-
 
 /*
     TODO
 
     Arreglar Dar de alta (no mete el proveedor en el producto)
-
     Mejorar codigo
 
  */
 
-
-
+/**
+ * Clase principal de la aplicación StockControl.
+ * Esta clase se encarga de gestionar el flujo principal de la aplicación y las interacciones con el usuario.
+ */
 public class AppStockControl {
 
+    // Instancias de servicios y controladores
     static DBConnection dbConnection = new DBConnection();
     static ProveedorRepository proveedorRepository = new ProveedorRepository(dbConnection);
     static ProveedorService proveedorService = new ProveedorService(proveedorRepository);
     static ProductoService productoService = new ProductoService();
     static ProveedorController proveedorController = new ProveedorController(proveedorService, productoService);
 
+    /**
+     * Método principal de la aplicación.
+     * Este método se encarga de gestionar el flujo principal de la aplicación, realizando el login
+     * y las operaciones de CRUD de productos y proveedores.
+     *
+     * @param args Argumentos de línea de comandos (no utilizados en esta aplicación).
+     */
     public static void main(String[] args) {
-
-        /* Inyecta los usuatios, si no existen, en la base de datos.
-         * Usuario : Elia
-         * Contrasenia: 123
-         * Usuario Sara
-         * Contraseña: 12
-         * Usuario: Manu
-         * Contraseña: 1
-         */
-        Initializer.usersInyection();
-
-        /*
-        Declaro aquí variables que voy a usar durante la ejecución del main
-         */
+        // Declaro aquí variables que voy a usar durante la ejecución del main
         Scanner scan = new Scanner(System.in);
         boolean login = false;  // Variable para comprobar si se hace un login correcto o no
         User user = new User(); // Variable para almacenar al usuario que se ha logado
 
         /*
-        1A PARTE. LOGIN
+         * Inyecta los usuarios, si no existen, en la base de datos.
+         *   Usuario : Elia - Contraseña: 123
+         *   Usuario : Sara - Contraseña: 12
+         *   Usuario : Manu - Contraseña: 1
+         */
+        Initializer.usersInyection();
 
-        EN ESTA PARTE SE REALIZA UN LOGIN EN LA APLICACIÓN.
-        SE PIDE EL USUARIO Y CONTRASENIA Y SE LLAMA AL METODO login DE LA CLASE UserController
+        /*
+         * Proceso de login:
+         * Se pedirá al usuario su nombre y contraseña, y se validará contra los datos existentes.
+         * Si el login es exitoso, se almacena al usuario logueado.
          */
         do {
-            // Ni caso a este try/catch
             try {
                 Thread.sleep(500);
             } catch (Exception e) {
@@ -110,18 +110,16 @@ public class AppStockControl {
 
 
         /*
-        2A PARTE. GESTION DE STOCK
-
-        EN ESTA PARTE SE REALIZA UN CRUD DE GESTION DE STOCK
-        1. Alta producto
-        2. Baja producto
-        3. Modificar nombre producto
-        4. Modificar stock producto
-        5. Get producto por id
-        6. Get productos con stock
-        7. Get productos sin stock
-        8. Get proveedores de un producto
-        9. Get todos los proveedores
+         * CRUD de gestión de Stock:
+         *  1. Alta producto
+         *  2. Baja producto
+         *  3. Modificar nombre producto
+         *  4. Modificar stock producto
+         *  5. Get producto por id
+         *  6. Get productos con stock
+         *  7. Get productos sin stock
+         *  8. Get proveedores de un producto
+         *  9. Get todos los proveedores
          */
 
         String opc;
@@ -189,6 +187,9 @@ public class AppStockControl {
 
     }
 
+    /**
+     * Realiza la operación de alta de un nuevo producto en la base de datos.
+     */
     public static void altaProducto() {
         Scanner scan = new Scanner(System.in);
         ProductoController productoController = new ProductoController();
@@ -222,7 +223,9 @@ public class AppStockControl {
 
     }
 
-
+    /**
+     * Realiza la operación de dar de baja un producto de la base de datos.
+     */
     public static void bajaProducto() {
         Scanner scan = new Scanner(System.in);
         ProductoController productoController = new ProductoController();
@@ -233,12 +236,15 @@ public class AppStockControl {
         RespuestaHTTP<Producto> respuesta = productoController.bajaProducto(idProducto);
 
         if (respuesta != null && respuesta.getCodigo() == 200) {
-            System.out.printf("OPERACION EXITOSA");
+            System.out.print("OPERACION EXITOSA");
         } else {
             System.out.printf("Error en la operacion\n\t-codigo %d\n\t-%s\n", respuesta.getCodigo(), respuesta.getMensaje());
         }
     }
 
+    /**
+     * Modifica el nombre de un producto existente en la base de datos.
+     */
     public static void modificarNombreProducto() {
         Scanner scan = new Scanner(System.in);
         ProductoController productoController = new ProductoController();
@@ -250,12 +256,15 @@ public class AppStockControl {
         RespuestaHTTP<Producto> respuesta = productoController.modificarNombreProducto(idProducto, nuevoNombre);
 
         if (respuesta != null && respuesta.getCodigo() == 200) {
-            System.out.printf("OPERACION EXITOSA");
+            System.out.print("OPERACION EXITOSA");
         } else {
             System.out.printf("Error en la operacion\n\t-codigo %d\n\t-%s\n", respuesta.getCodigo(), respuesta.getMensaje());
         }
     }
 
+    /**
+     * Modifica el stock actual de un producto existente de la base de datos.
+     */
     public static void modificarStockProducto() {
         Scanner scan = new Scanner(System.in);
         ProductoController productoController = new ProductoController();
@@ -268,12 +277,15 @@ public class AppStockControl {
         RespuestaHTTP<Producto> respuesta = productoController.modificarStockProducto(idProducto, nuevoStock);
 
         if (respuesta != null && respuesta.getCodigo() == 200) {
-            System.out.printf("OPERACION EXITOSA");
+            System.out.print("OPERACION EXITOSA");
         } else {
             System.out.printf("Error en la operacion\n\t-codigo %d\n\t-%s\n", respuesta.getCodigo(), respuesta.getMensaje());
         }
     }
 
+    /**
+     * Obtiene la información de un producto existente en la base de datos por su identificador.
+     */
     public static void getProductoPorId() {
         Scanner scan = new Scanner(System.in);
         ProductoController productoController = new ProductoController();
@@ -293,6 +305,9 @@ public class AppStockControl {
 
     }
 
+    /**
+     * Obtiene una lista de todos los productos existentes que tengan stock mayor a 0.
+     */
     public static void getProductosConStock() {
         ProductoController productoController = new ProductoController();
         System.out.println("6. Get productos con stock");
@@ -310,6 +325,9 @@ public class AppStockControl {
 
     }
 
+    /**
+     * Obtiene una lista de todos los productos existentes cuyo stock sea 0.
+     */
     public static void getProductosSinStock() {
         ProductoController productoController = new ProductoController();
         System.out.println("7. Get productos sin stock");
@@ -327,6 +345,10 @@ public class AppStockControl {
         }
     }
 
+
+    /**
+     * Obtiene una lista de proveedores que suministran un producto concreto.
+     */
     public static void getProveedoresDeUnProducto() {
         Scanner scan = new Scanner(System.in);
 
@@ -347,6 +369,9 @@ public class AppStockControl {
 
     }
 
+    /**
+     * Obtiene una lista de todos los proveedores registrados en la base de datos.
+     */
     public static void getTodosLosProveedores() {
 
         System.out.println("9. Get todos los proveedores");
